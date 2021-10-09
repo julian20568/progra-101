64ca5dd84794b0cf421e8d496ea8590a19c21aa2
julian20568 <jaop17@hotmail.com>
2021-10-09 18:00:38 -0500

    [Add] Subiendo mi proyecto al GitHub

---
+++ seccion-3/app.js
@@ -0,0 +1,13 @@
+//console.log(' Hola Mundo');
+
+//Convertir grados F a C°
+
+let c;
+let f = 90;
+
+//c = ( f - 32) * ( 5 / 9 ); // 1ra forma
+
+c = f - 32;
+c = c * (5 / 9); // 2da forma
+
+console.log(c);
