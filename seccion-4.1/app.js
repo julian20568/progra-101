7608cd2dcd59b659521f70c5669a33fbf67123ee
julian20568 <jaop17@hotmail.com>
2021-10-09 19:09:55 -0500

    [Add] Se agrega commit de condicionales

---
+++ seccion-4.1/app.js
@@ -0,0 +1,19 @@
+//Crear un algoritmo, que basado en la nota
+//imprima si el alumno aprueba la materia
+//aprueba 60
+//si el alumno tiene nota  menor a 50
+//imprima un mensaje que diga
+//por favor estudie mas
+
+let nota =  40;
+
+if ( nota >= 60 ){
+    console.log('Aprobado');
+} else {
+    
+    if ( nota < 50) {
+        console.log('Por favor estudie más');
+    } else {
+        console.log('No aprobado');
+    }
+} 
---
+++ seccion-4/app.js
@@ -0,0 +1,14 @@
+let nombre = 'Julián';
+let apellido = 'Obando';
+
+let nombreCompleto = nombre + ' ' + apellido;
+
+console.log(nombreCompleto);
+
+let a = 10;
+let b = 20;
+
+console.log(a + b)
+
+let activo = true;
+let despedido = false;
